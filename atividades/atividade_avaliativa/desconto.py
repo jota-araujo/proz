c3c124b108e12548d9ca24571e1f682d69884060
João Araújo <ar4ujo.dev@gmail.com>
2025-05-22 11:12:53 -0300

    Create desconto.py

---
+++ atividades/atividade_avaliativa/desconto.py
@@ -0,0 +1,4 @@
+valor = float(input("Digite o valor do produto: "))
+percentual = int(input("Digite a porcentagem de desconto:"))
+desconto = valor-(valor*percentual)/100
+print("O valor do produto com desconto é de:",desconto)
