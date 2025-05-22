6ece87f28e4b584e2ccbfe7ef7843de8360ec9a3
João Araújo <ar4ujo.dev@gmail.com>
2025-05-22 11:10:06 -0300

    Create preco_pagarr.py

---
+++ atividades/atividade_avaliativa/preco_pagarr.py
@@ -0,0 +1,4 @@
+cafe = int(input("Qual o valor do café: "))
+quantidade_cafe = int(input("Quantos cafés você deseja: "))
+total = quantidade_cafe*cafe
+print("O valor total do seu pedido é: R$",total)
