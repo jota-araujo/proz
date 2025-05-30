f1c2a7ecabf0b5eb0a88feeacb6144912d6c5246
João Araújo <ar4ujo.dev@gmail.com>
2025-05-30 08:47:50 -0300

    Update and rename IMC.py to area_retanguloo.py

--- atividades/atividade_avaliativa/area_retanguloo.py
+++ atividades/atividade_avaliativa/area_retanguloo.py
@@ -1,4 +1,4 @@
-altura = float(input("Digite a altura do retângulo: "))
+altura = float(input("Digite a altura do retângulo: ")) 
 largura = float(input("Digite a largura do retângulo: "))
 area = altura*largura
 print("A área do retângulo é:",area)
